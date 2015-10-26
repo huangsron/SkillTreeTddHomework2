778f462f3c2cb83bbb926d7ef54a48be7e18349a
sron <sron@linkensys.com>
2015-10-26 11:48:11 +0800

    added 3 diff book dicsount

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace PotterShoppingCart.Tests
@@ -23,6 +22,7 @@ namespace PotterShoppingCart.Tests
             };
         }
 
+        //Scenario: 第一集買了一本，其他都沒買，價格應為100*1=100元
         [TestMethod]
         public void Test_buy_1_book_return_amount_100()
         {
@@ -64,6 +64,27 @@ namespace PotterShoppingCart.Tests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_buy_3_diff_book_return_amount_270()
+        {
+            var orders = new List<Order>
+            {
+                new Order {Id = 1, Quantity = 1,Price = 100},
+                new Order {Id = 2, Quantity = 1,Price = 100},
+                new Order {Id = 3, Quantity = 1,Price = 100},
+                new Order {Id = 4, Quantity = 0,Price = 100},
+                new Order {Id = 5, Quantity = 0,Price = 100},
+            };
+
+            var target = new ShoppingCart(_book);
+
+            var expected = 270;
+
+            var actual = target.GetAmount(orders);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public class ShoppingCart
@@ -83,14 +104,13 @@ namespace PotterShoppingCart.Tests
             var count = 0;
             double amount = 0;
 
-
-            while (orders.Any(e=>e.Quantity > count))
+            while (orders.Any(e => e.Quantity > count))
             {
                 var temp = orders.Where(e => e.Quantity > count).ToList();
 
                 var discount = GetDiscount(temp);
 
-                amount += temp.Sum(e => (e.Price*discount));
+                amount += temp.Sum(e => (e.Price * discount));
 
                 count++;
             }
@@ -106,8 +126,13 @@ namespace PotterShoppingCart.Tests
                 case 1:
                     //當資料只有一筆不打折
                     return 1;
+
                 case 2:
                     return 0.95;
+
+                case 3:
+                    return 0.9;
+
                 default:
                     return 1;
             }
@@ -119,6 +144,7 @@ namespace PotterShoppingCart.Tests
         public int Id { get; set; }
 
         public int Quantity { get; set; }
+
         public int Price { get; set; }
     }
 
@@ -130,6 +156,4 @@ namespace PotterShoppingCart.Tests
 
         public int Price { get; set; }
     }
-
-   
 }
